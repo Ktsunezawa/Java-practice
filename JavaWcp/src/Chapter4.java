413a374f622903606525bc8489cca3c9af28ad70
Ktsunezawa <rapurasnohako@gmail.com>
2021-04-13 15:14:46 +0900

    Finish Chapter4

---
+++ JavaWcp/src/Chapter4.java
@@ -0,0 +1,15 @@
+
+public class Chapter4 {
+	public static void main(String[] args) {
+		String name;
+		name = "KoheiTsunezawa";
+		System.out.println(name);
+
+		name = "Make you happy";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER =7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
